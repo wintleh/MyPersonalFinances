b3cb41af44adae2d1897a0223063ed8c91e00f82
wintleh <hunter.wintle@gmail.com>
2019-12-08 17:18:02 -0500

    Add ability to serve html page
    Add static files (html, js, images)
    Serve basic page

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 'use strict'
 
+const path = require('path');
+
 // Read environment variables
 const env = require('./src/env/env');
 
 const fastify = require('fastify')({
     ignoreTrailingSlash: true,
     logger: true
-})
+});
+
+// Fastify extension to serve static files
+fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, '/src/static')
+});
 
 // Register routes
 fastify.register(require('./src/routes/root/root'));
 fastify.register(require('./src/routes/api/bank/bank'), { prefix: '/api/bank'}); // UPDATE
 
+// Get the host and port from environment vars
 const serverOptions = {
     host: env.HOST,
     port: env.PORT
-}
+};
 
 // Start server, get port
 fastify.listen(serverOptions, (err, address) => {
